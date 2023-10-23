00c739d6851ad599850205b603c95e2117f0a41d
Anmol Aman <81010438+anmolaman20@users.noreply.github.com>
2023-10-24 03:45:43 +0530

    Time: 0 ms (100%), Space: 6.5 MB (13.76%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -26,30 +26,35 @@
 
 
 
-//Observation on bits
-class Solution {
-public:
-    bool isPowerOfFour(int n) {
-        int cntSet = 0, cntSetPos, tPos = 1;
-
-        while(n){
-            if(n&1){
-                cntSet++;
-                cntSetPos=tPos;
-            }
-            if(cntSet>1) return false;
-            n>>=1;
-            tPos++;
-        }
-        return cntSet==1 and (cntSetPos%2);
-    }
-};
-
-
-// //bitmanipulation
+// //Observation on bits
 // class Solution {
 // public:
 //     bool isPowerOfFour(int n) {
-        
+//         int cntSet = 0, cntSetPos, tPos = 1;
+
+//         while(n){
+//             if(n&1){
+//                 cntSet++;
+//                 cntSetPos=tPos;
+//             }
+//             if(cntSet>1) return false;
+//             n>>=1;
+//             tPos++;
+//         }
+//         return cntSet==1 and (cntSetPos%2);
 //     }
 // };
+
+
+//bitmanipulation
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0) return false;
+
+        for(int i=0;i<32;i+=2){
+            if(n==(1<<i)) return true;
+        }
+        return false;
+    }
+};
