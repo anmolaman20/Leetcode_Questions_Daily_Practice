06a97cd0c7ba6469d1446b16d6018b56617f20e2
Anmol Aman <81010438+anmolaman20@users.noreply.github.com>
2023-10-26 13:11:33 +0530

    Time: 172 ms (30.41%), Space: 130.2 MB (84.93%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n = nums.size();
-        int windowSize = 2 * k + 1;
-        vector<int> ans(n, -1);
+        int wsz = 2 * k + 1;
+        vector<int> ans(n,-1);
+        if(n<wsz) return ans;
 
-        if (n < windowSize) {
-            return ans;
+        long long sum = 0;
+        int head = 0, tail = 0;
+
+        for(head=0;head<n;head++){
+            sum += nums[head];
+            while(head-tail+1 > wsz){
+                sum-=nums[tail];
+                tail++;
+            }
+
+
+            if(head-tail+1 == wsz){
+                ans[head-k] = sum / wsz;
+            }
         }
-
-        vector<long long> prefixSum(n + 1);
-        for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
-        }
-
-        for (int i = k; i + k < n; ++i) {
-            ans[i] = (prefixSum[i + k + 1] - prefixSum[i - k]) / windowSize;
-        }
-
         return ans;
     }
 };
