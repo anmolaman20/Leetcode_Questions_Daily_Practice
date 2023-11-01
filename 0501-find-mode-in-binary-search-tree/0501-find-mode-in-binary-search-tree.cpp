0f731481fcb41a0b1c5a7ceb8402ae0e43d12fd0
Anmol Aman <81010438+anmolaman20@users.noreply.github.com>
2023-11-01 15:01:01 +0530

    Time: 12 ms (84.62%), Space: 24.3 MB (91.46%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,21 +11,50 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    int mxfreq = INT_MIN;
-    void preOrder(TreeNode* root){
+    int currFreq = 0, mxFreq = 0, prevVal = INT_MIN;
+    vector<int> ans;
+    void f(TreeNode* root){
         if(!root) return;
-        mp[root->val]++;
-        mxfreq = max(mxfreq,mp[root->val]);
-        preOrder(root->left);
-        preOrder(root->right);
+        f(root->left);
+        if(prevVal == root->val) currFreq++;
+        else currFreq = 1;
+        if(currFreq>mxFreq){
+            ans.clear();
+            ans.push_back(root->val);
+            mxFreq = currFreq;
+        }else if(currFreq==mxFreq){
+            ans.push_back(root->val);
+        }
+        prevVal = root->val;
+        f(root->right);
+
     }
     vector<int> findMode(TreeNode* root) {
-        vector<int> ans;
-        preOrder(root);
-        for(auto& it : mp){
-            if(it.second==mxfreq) ans.push_back(it.first);
-        }
+        f(root);
         return ans;
     }
 };
+
+
+
+
+//class Solution {
+// public:
+//     unordered_map<int,int> mp;
+//     int mxfreq = INT_MIN;
+//     void preOrder(TreeNode* root){
+//         if(!root) return;
+//         mp[root->val]++;
+//         mxfreq = max(mxfreq,mp[root->val]);
+//         preOrder(root->left);
+//         preOrder(root->right);
+//     }
+//     vector<int> findMode(TreeNode* root) {
+//         vector<int> ans;
+//         preOrder(root);
+//         for(auto& it : mp){
+//             if(it.second==mxfreq) ans.push_back(it.first);
+//         }
+//         return ans;
+//     }
+// };
